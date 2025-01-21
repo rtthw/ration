99d1d4c2fc6763fe685910e278ddc89090bfa315
rtthw <172864013+rtthw@users.noreply.github.com>
2025-01-21 10:06:25 -0500

    Implement block creation

--- src/block.rs
+++ src/block.rs
@@ -2,22 +2,56 @@
 
 
 
-use crate::Result;
+use std::path::Path;
+
+use crate::{Error, Result};
 
 
 
 /// A typed, shared block of memory.
-pub struct Block<T> {
+pub struct Block<T: Sized> {
     shm: shared_memory::Shmem,
     ptr: *mut T,
 }
 
-impl<T> Block<T> {
-    pub fn alloc() -> Result<Self> {
-        todo!()
+impl<T: Sized> Block<T> {
+    /// Allocate a new shared block of memory at the given path, and of the given type.
+    pub fn alloc(path: impl AsRef<Path>) -> Result<Self> {
+        let size = std::mem::size_of::<T>();
+        let shm = match shared_memory::ShmemConf::new().flink(&path).size(size).create() {
+            Ok(shmem) => shmem,
+            Err(shared_memory::ShmemError::LinkExists) => {
+                return Err(Error::BlockAlreadyAllocated);
+            }
+            Err(e) => { return Err(Error::Shm(e)); }
+        };
+
+        let ptr = shm.as_ptr() as *mut T;
+
+        Ok(Self {
+            shm,
+            ptr,
+        })
     }
 
-    pub fn open() -> Result<Self> {
-        todo!()
+    /// Open a shared block of memory identified by the given path and type.
+    pub fn open(path: impl AsRef<Path>) -> Result<Self> {
+        let shm = shared_memory::ShmemConf::new()
+            .flink(path)
+            .open()
+            .map_err(|e| Error::Shm(e))?;
+
+        // Check if the expected type's size matches the allocated block's size.
+        let size = std::mem::size_of::<T>();
+        if shm.len() != size {
+            return Err(Error::InvalidBlockSize);
+        }
+
+        let ptr = shm.as_ptr() as *mut T;
+
+        Ok(Self {
+            shm,
+            ptr,
+        })
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,5 +8,10 @@ pub mod block;
 pub type Result<T> = std::result::Result<T, Error>;
 
 pub enum Error {
+    /// Generic shared memory error.
     Shm(shared_memory::ShmemError),
+    /// Expected a different block size from the one that was given.
+    InvalidBlockSize,
+    /// Attempted to allocated a block that has already been allocated.
+    BlockAlreadyAllocated,
 }
