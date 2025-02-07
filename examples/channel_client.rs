b33b73c0bcdc3ec5d1bf0f51ea2231c849e14257
rtthw <172864013+rtthw@users.noreply.github.com>
2025-02-07 13:53:46 -0500

    Add channel example

---
+++ examples/channel_client.rs
@@ -0,0 +1,29 @@
+
+
+
+use std::time;
+
+use iproc::Array;
+
+
+
+fn main() {
+    let mut client_array: Array<char> = Array::alloc("/tmp/CHANNEL_CLIENT", 64).unwrap();
+    let mut server_array: Array<char> = Array::open("/tmp/CHANNEL_SERVER").unwrap();
+
+    let start_time = time::Instant::now();
+
+    let mut char_iter = "thisisatest".chars(); // More than 5 characters long.
+    loop {
+        // Make sure the client doesn't just run forever.
+        if time::Instant::now().duration_since(start_time) >= time::Duration::from_secs(3) {
+            println!("CLIENT: Done!");
+            break;
+        }
+        // Check the server
+        if let Some(ch) = server_array.pop() {
+            println!("CLIENT: Sending message #{ch}...");
+            client_array.push(char_iter.next().unwrap());
+        }
+    }
+}
---
+++ examples/channel_server.rs
@@ -0,0 +1,38 @@
+
+
+
+use iproc::Array;
+
+
+
+fn main() {
+    let mut server_array: Array<char> = Array::alloc("/tmp/CHANNEL_SERVER", 64).unwrap();
+
+    let mut client = std::process::Command::new("cargo")
+        .args(["run", "--example", "channel_client"])
+        .spawn()
+        .unwrap();
+
+    // Give the client some time to start. This should be enough.
+    std::thread::sleep(std::time::Duration::from_secs(2));
+
+    // Wait to open the client array until the client has allocated it.
+    let mut client_array: Array<char> = Array::open("/tmp/CHANNEL_CLIENT").unwrap();
+
+    let mut msg_count = 0;
+    server_array.push(char::from_digit(msg_count, 10).unwrap());
+    loop {
+        // Only process the first 5 messages.
+        if msg_count >= 5 {
+            println!("SERVER: Done!");
+            let _ = client.wait().unwrap();
+            break;
+        }
+        // Process the next message, if there is any.
+        if let Some(client_message) = client_array.pop() {
+            println!("SERVER: Received message '{}' from client.", client_message);
+            msg_count += 1;
+            server_array.push(char::from_digit(msg_count, 10).unwrap());
+        }
+    }
+}
