a835cf325a612b90dc573bda456097d8a7b030ea
rtthw <172864013+rtthw@users.noreply.github.com>
2025-06-09 06:30:31 -0400

    Move to nightly for allocator crate

--- ration-allocator/src/lib.rs
+++ ration-allocator/src/lib.rs
@@ -1,1 +1,25 @@
 //! Ration shared memory allocator
+
+#![feature(allocator_api)]
+
+
+
+use core::{
+    alloc::{Layout, AllocError},
+    ptr::NonNull,
+};
+
+
+
+#[derive(Clone)]
+pub struct SharedAllocator;
+
+unsafe impl core::alloc::Allocator for SharedAllocator {
+    fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
+        todo!()
+    }
+
+    unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
+        todo!()
+    }
+}
